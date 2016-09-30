7a8558caeb945b7f5315dacb37fb8f6fb192c3ad
ann_ann <saddy666@gmail.com>
2016-09-30 13:27:28 +0300

    switch back to geocoder

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -8,18 +8,18 @@ class Invite < ApplicationRecord
   validates :location,    presence: true
   validate  :date_is_valid_datetime
 
-  before_save :get_lat_lng
+  # before_save :get_lat_lng
 
   def date_is_valid_datetime
     errors.add(:date, 'must be a valid datetime') if ((date.to_datetime rescue ArgumentError) == ArgumentError)
   end
 
-  def get_lat_lng
-    full_address = [self.location, self.city, self.country].reject(&:nil?).join(' ')
-    if (self.new_record? || self.location_changed? || self.city_changed) && full_address.present?
-      a = Geokit::Geocoders::GoogleGeocoder.geocode(full_address)
-      self.latitude = a.lat
-      self.longitude = a.lng
-    end
-  end
+  # def get_lat_lng
+  #   full_address = [self.location, self.city, self.country].reject(&:nil?).join(' ')
+  #   if (self.new_record? || self.location_changed? || self.city_changed?) && full_address.present?
+  #     a = Geokit::Geocoders::GoogleGeocoder.geocode(full_address)
+  #     self.latitude = a.lat
+  #     self.longitude = a.lng
+  #   end
+  # end
 end
